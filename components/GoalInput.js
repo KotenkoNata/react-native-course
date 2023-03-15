af58a420a00c4e885460521d0c12f7341fa99da2
KotenkoNata <kotenkonata8@gmail.com>
2023-03-15 16:32:13 +0200

    feat: styling the Modal Overlay

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -18,7 +18,14 @@ function GoalInput(props) {
                            onChangeText={goalInputHandler}
                            value={enteredGoalText}
                 />
-                <Button title='Add goal' onPress={addGoalHandler}/>
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title='Add goal' onPress={addGoalHandler}/>
+                    </View>
+                    <View style={styles.button}>
+                        <Button title='Cancel' />
+                    </View>
+                </View>
             </View>
         </Modal>
         );
@@ -29,18 +36,26 @@ export default GoalInput;
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 24,
+        padding: 16,
         borderBottomWidth: 1,
         borderBottomColor: '#ccc'
     },
     textInput:{
         borderWidth: 1,
         borderColor: '#ccc',
-        width: '70%',
-        marginRight: 8,
+        width: '100%',
         padding: 8,
     },
+    buttonContainer: {
+        marginTop: 16,
+        flexDirection: 'row',
+
+    },
+    button: {
+        width: 100,
+        marginHorizontal: 8
+    }
 });
