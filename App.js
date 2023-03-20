53d815f181bb826c76534e02263b8eade18415bb
KotenkoNata <kotenkonata8@gmail.com>
2023-03-20 19:35:53 +0200

    feat: respecting device screen restrictions with the safeAreaView

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {StyleSheet, ImageBackground} from 'react-native';
+import {StyleSheet, ImageBackground, SafeAreaView} from 'react-native';
 import StartGameScreen from "./screens/StartGameScreen";
 import {LinearGradient} from "expo-linear-gradient";
 import {useState} from "react";
@@ -20,13 +20,16 @@ export default function App() {
 
   return (
       <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
-            <ImageBackground
+
+          <ImageBackground
                 source={require('./assets/images/bg.jpg')}
                 resizeMode='cover'
                 style={styles.rootScreen}
                 imageStyle={styles.backgroundImage}
             >
-                {screen}
+              <SafeAreaView style={styles.rootScreen}>
+                  {screen}
+              </SafeAreaView>
             </ImageBackground>
       </LinearGradient>
   );
