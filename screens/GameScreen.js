a1c29583ac54646861bbff341a055fdc750111e6
KotenkoNata <kotenkonata8@gmail.com>
2023-03-24 11:36:44 +0200

    feat: using cascading styles

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -54,10 +54,14 @@ function GameScreen({userNumber, onGameOver}) {
             <Title>Opponent's Guess</Title>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card>
-                <InstructionText>Higher or lower?</InstructionText>
-                <View>
-                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+                <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
                     <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+                    </View>
                 </View>
             </Card>
             <View>
@@ -74,5 +78,13 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 24,
     },
-
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1,
+    },
+    instructionText: {
+        marginBottom: 12,
+    }
 })
