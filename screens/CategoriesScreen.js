4a7b3553b445c844e8dca9c39bfc3daf7c9801bc
KotenkoNata <kotenkonata8@gmail.com>
2023-03-29 19:15:47 +0300

    feat: Implementing Navigation Between Two Screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,11 +1,20 @@
 import {CATEGORIES} from "../data/dummy-data";
 import {FlatList} from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
+import MealsOverviewScreen from "./MealsOverviewScreen";
 
-function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-}
-function CategoriesScreen() {
+
+function CategoriesScreen({navigation}) {
+    function renderCategoryItem(itemData) {
+
+        function pressHandler() {
+            navigation.navigate('MealsOverview')
+        }
+
+        return <CategoryGridTile title={itemData.item.title}
+                                 color={itemData.item.color}
+                                 onPress={pressHandler}/>
+    }
 
     return (
            <FlatList data={CATEGORIES}
