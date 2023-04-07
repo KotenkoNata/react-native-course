753f73267aba7a96cbad9bd3f4fcc5cc2d28e055
KotenkoNata <kotenkonata8@gmail.com>
2023-04-07 16:56:06 +0300

    feat: getting started with Redux & Redux Toolkit

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import MealDetailScreen from "./screens/MealDetailScreen";
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import FavoritesScreen from "./screens/FavoritesScreen";
 import {Ionicons} from '@expo/vector-icons'
-import FavoritesContextProvider from "./store/context/favorites-context";
+import {Provider} from "react-redux";
+import {store} from "./store/redux/store"
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -37,7 +38,8 @@ export default function App() {
   return (
       <>
         <StatusBar style='light' />
-          <FavoritesContextProvider>
+          {/*<FavoritesContextProvider>*/}
+          <Provider store={store}>
               <NavigationContainer>
                   <Stack.Navigator screenOptions={{
                       headerStyle: {backgroundColor: '#351401'},
@@ -60,7 +62,8 @@ export default function App() {
                       />
                   </Stack.Navigator>
               </NavigationContainer>
-          </FavoritesContextProvider>
+          </Provider>
+          {/*</FavoritesContextProvider>*/}
       </>
   );
 }
