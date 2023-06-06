e22e20ade289f6081c624f2af4d75cbdd40f72bc
KotenkoNata <kotenkonata8@gmail.com>
2023-06-06 16:26:00 +0300

    feat: add navigation and configure navigation

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import ManageExpense from "./screens/ManageExpense";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+
+const Stack = createStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return <BottomTabs.Navigator>
+    <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+    <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+  </BottomTabs.Navigator>
+}
+export default function App() {
+  return (
+      <>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+            <Stack.Screen name="ManageExpense" component={ManageExpense}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
+  );
+}
+
