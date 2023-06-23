20581da316f184db4d8381afee5c8a787745c2d5
KotenkoNata <kotenkonata8@gmail.com>
2023-06-23 20:19:38 +0300

    feat: Manage Form State & Submission

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,8 +1,9 @@
 import {StyleSheet, Text, View} from "react-native";
 import Input from "./Input";
 import {useState} from "react";
+import Button from "../UI/Button";
 
-function ExpenseForm() {
+function ExpenseForm({onCancel, submitButtonLabel}) {
     const [inputValue, setInputValue] = useState({
         amount: "",
         date: "",
@@ -16,6 +17,10 @@ function ExpenseForm() {
                 [inputIdentifier]: enteredValue,
             }
         });
+    }
+    
+    function submitHandler() {
+        
     }
     
     return <View style={styles.form}>
@@ -38,6 +43,10 @@ function ExpenseForm() {
             onChangeText: inputChangeHandler.bind(this, "description"),
             value: inputValue.description,
         }}/>
+        <View style={styles.buttons}>
+            <Button style={styles.button} mode="flat" onPress={onCancel}>Cancel</Button>
+            <Button style={styles.button} onPress={submitHandler}>{submitButtonLabel}</Button>
+        </View>
     </View>
 }
 
@@ -60,5 +69,14 @@ const styles = StyleSheet.create({
     },
     rowInput: {
         flex: 1,
-    }
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8,
+    },
 })
