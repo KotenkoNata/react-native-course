23724385038b7a9c8d4c1e06e001d75d05a2ff3a
KotenkoNata <kotenkonata8@gmail.com>
2023-06-26 16:27:55 +0300

    feat: Send POST Http Requests to Firebase

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -5,6 +5,7 @@ import {GlobalStyles} from "../constants/styles";
 import Button from "../components/UI/Button";
 import {ExpensesContext} from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import {storeExpense} from "../utils/http";
 
 function ManageExpense({route, navigation}) {
     const expensesCtx = useContext(ExpensesContext);
@@ -36,6 +37,7 @@ function ManageExpense({route, navigation}) {
                 expenseData,
             );
         } else {
+            storeExpense(expenseData)
             expensesCtx.addExpense(expenseData);
         }
         navigation.goBack();
