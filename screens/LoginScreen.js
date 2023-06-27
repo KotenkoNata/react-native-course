07fe50a6c8b62cce806ceb38760075f451337c09
KotenkoNata <kotenkonata8@gmail.com>
2023-06-27 19:29:21 +0300

    feat: Authentication Error Handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,13 +2,20 @@ import AuthContent from '../components/Auth/AuthContent';
 import {useState} from "react";
 import {login} from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import {Alert} from "react-native";
 
 function LoginScreen() {
     const [isAuthenticating, setIsAuthenticating] = useState(false);
 
     async function loginHandler({email, password}) {
         setIsAuthenticating(true);
-        await login(email, password);
+        try {
+            await login(email, password);
+        }catch (error){
+            Alert.alert('Authentication Failed',
+                'Could not log you in. Please check your credentials or try again later')
+        }
+
         setIsAuthenticating(false);
     }
 
--- util/auth.js
+++ util/auth.js
@@ -10,8 +10,6 @@ export async function authenticate(mode, email, password) {
         password: password,
         returnSecureToken: true,
     })
-
-    console.log(response.data)
 }
 export async function createUser(email, password) {
     await authenticate('signUp', email, password)
