09f2b8cdd030391e53278f8ff51919676a2753ff
KotenkoNata <kotenkonata8@gmail.com>
2023-06-27 16:23:57 +0300

    feat: Update & Delete Backend Data (Requests)

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -5,7 +5,7 @@ import {GlobalStyles} from "../constants/styles";
 import Button from "../components/UI/Button";
 import {ExpensesContext} from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
-import {storeExpense} from "../utils/http";
+import {deleteExpense, storeExpense, updateExpense} from "../utils/http";
 
 function ManageExpense({route, navigation}) {
     const expensesCtx = useContext(ExpensesContext);
@@ -21,7 +21,8 @@ function ManageExpense({route, navigation}) {
         })
     }, [navigation, isEditing])
 
-    function deleteExpenseHandler() {
+    async function deleteExpenseHandler() {
+        await deleteExpense(editedExpenseId);
         expensesCtx.deleteExpense(editedExpenseId);
         navigation.goBack();
     }
@@ -36,6 +37,7 @@ function ManageExpense({route, navigation}) {
                 editedExpenseId,
                 expenseData,
             );
+            await updateExpense(editedExpenseId, expenseData);
         } else {
             const id = await storeExpense(expenseData);
             expensesCtx.addExpense({...expenseData, id: id});
