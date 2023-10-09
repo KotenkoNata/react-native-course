d2df73d85313f28ce72eeebd46576bda91fbf2c0
KotenkoNata <kotenkonata8@gmail.com>
2023-10-09 16:04:40 +0200

    feat: Getting Started with the Location Picker

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -2,6 +2,7 @@ import {View, Text, ScrollView, TextInput, StyleSheet} from "react-native";
 import {useState} from "react";
 import {Colors} from "../../contants/colors";
 import ImagePicker from "./ImagePicker";
+import LocationPicker from "./LocationPicker";
 function PlaceForm() {
     const [enteredTitle, setEnteredTitle, ] = useState();
     
@@ -17,6 +18,7 @@ function PlaceForm() {
                        value={enteredTitle}/>
         </View>
         <ImagePicker />
+        <LocationPicker />
     </ScrollView>
 }
 
