e506aee9e790db7a527049f2822f89cda4edfc26
KotenkoNata <kotenkonata8@gmail.com>
2023-11-01 10:29:00 +0100

    feat: Fetching Places from the Database

--- util/database.js
+++ util/database.js
@@ -1,4 +1,5 @@
 import * as SQLite from 'expo-sqlite';
+import {Place} from "../models/place";
 
 const database = SQLite.openDatabase('places.db');
 
@@ -31,6 +32,30 @@ export function insertPlace(place) {
                     reject(error);
                 }
                 )
+        })
+    })
+
+    return promise;
+}
+
+export function fetchPlaces() {
+    const promise = new Promise((resolve, reject)=>{
+        database.transaction((tx)=>{
+            tx.executeSql(`SELECT * FROM places`,
+                [],
+                (_, result)=>{
+                const places = [];
+                for (const dp of result.rows._array){
+                    places.push(new Place(dp.title, dp.imageUri, {
+                            address: dp.address,
+                            lat: dp.lat,
+                            lng: dp.lng}, dp.id))
+                }
+                resolve(places);
+                },
+                (_,error)=>{
+                reject(error);
+                });
         })
     })
 
