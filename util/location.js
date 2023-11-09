de119837d475e2ea678754a062af5acc060df93f
KotenkoNata <kotenkonata8@gmail.com>
2023-11-09 14:55:43 +0100

    feat: update keys

--- util/location.js
+++ util/location.js
@@ -1,11 +1,11 @@
-const GOOGLE_API_KEY = 'AIzaSyBbSSN1BDOtkEOO-w7418n81KIuKRTUn00'
+const key = process.env.EXPO_GOOGLE_API_KEY;
 
 export function getMapPreview(lat, lng) {
-    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${key}`;
 }
 
 export async function getAddress(lat, lng) {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${key}`
     const response = await fetch(url);
 
     if(!response.ok){
