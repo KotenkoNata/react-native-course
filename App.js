e7237aaacb1758431dfb46c2b81b8ac514e5958a
KotenkoNata <kotenkonata8@gmail.com>
2023-11-14 15:12:13 +0100

    feat: Local Notifications - Permissions

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
 import * as Notifications from 'expo-notifications';
 
+Notifications.setNotificationHandler({
+  handleNotification: async ()=>{
+    return{
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true
+    }
+  }
+});
+
 export default function App() {
+
   function scheduleNotificationHandler() {
     Notifications.scheduleNotificationAsync({
-      content: {title: 'My first local notification',
+      content: {
+        title: 'My first local notification',
         body: 'This is the body of the notification.',
         data: {userName: 'Max'}
       },
@@ -16,7 +28,8 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Button title='Schedule Notification' onPress={scheduleNotificationHandler}/>
+      <Button title='Schedule Notification'
+              onPress={scheduleNotificationHandler}/>
       <StatusBar style="auto" />
     </View>
   );
