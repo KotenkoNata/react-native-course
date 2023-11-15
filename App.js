e49dd7fc3b3a3a329ac4c46f96af4e1d0581010d
KotenkoNata <kotenkonata8@gmail.com>
2023-11-15 15:05:04 +0100

    feat: Push Notifications Setup

--- App.js
+++ App.js
@@ -16,6 +16,12 @@ Notifications.setNotificationHandler({
 
 export default function App() {
 
+  useEffect(()=>{
+    Notifications.getExpoPushTokenAsync().then(pushTokenData=>{
+      console.log(`pushTokenData`,pushTokenData)
+    });
+  },[])
+
  useEffect(()=>{
    const subscription1 = Notifications.addNotificationReceivedListener((notification)=>{
      console.log(`NOTIFICATION RECEIVED`, notification);
