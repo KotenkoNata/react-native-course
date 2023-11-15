e30a23a29b0844aa66a4c5c4e58acf95ff70a7f4
KotenkoNata <kotenkonata8@gmail.com>
2023-11-15 20:03:47 +0100

    feat: Sending Push Notifications

--- App.js
+++ App.js
@@ -33,7 +33,6 @@ export default function App() {
       const pushTokenData = await Notifications.getExpoPushTokenAsync({
         projectId: Constants.expoConfig.extra.eas.projectId,
       });
-      console.log(pushTokenData);
       if(Platform.OS === 'android'){
         await Notifications.setNotificationChannelAsync('default',{
           name: 'default',
@@ -78,10 +77,26 @@ export default function App() {
       }
     });
   }
+  
+  function sendPushNotificationHandler() {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        to: process.env.EXPO_PUBLIC_API_KEY,
+        title: 'Test - sent from a device!',
+        body: 'This is a test!'
+      })
+    });
+  }
   return (
     <View style={styles.container}>
       <Button title='Schedule Notification'
               onPress={scheduleNotificationHandler}/>
+      <Button title='Send Push Notification'
+              onPress={sendPushNotificationHandler}/>
       <StatusBar style="auto" />
     </View>
   );
